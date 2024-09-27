38eb2c9f509825384cb918a8b7f628d50eb0b574
VictorCancino <juanvictorcancino@gmail.com>
2024-09-26 21:01:18 -0600

    Se agregarón nuevas consultas

--- primer_sql.py
+++ primer_sql.py
@@ -12,16 +12,30 @@ CREATE TABLE IF NOT EXISTS usuarios (
 )
 ''')
 
-cursor.execute("INSERT INTO usuarios (nombre,edad) VALUES ('Juan', 23)")
-cursor.execute("INSERT INTO usuarios (nombre,edad) VALUES ('Matias', 22)")
-cursor.execute("INSERT INTO usuarios (nombre,edad) VALUES ('Laura', 25)")
 
-conn.commit()
+#CONSULTA 01: AGREGAR DATOS A LA BASE DE DATOS
+#cursor.execute("INSERT INTO usuarios (nombre,edad) VALUES ('Juan', 23)")
+#cursor.execute("INSERT INTO usuarios (nombre,edad) VALUES ('Matias', 22)")
+#cursor.execute("INSERT INTO usuarios (nombre,edad) VALUES ('Laura', 25)")
+#conn.commit()
 
+#CONSULTA 02: USUARIOS CON EDAD IGUAL 30 AÑOS
+cursor.execute("SELECT * FROM usuarios WHERE edad = 30")
+usuarios_30 = cursor.fetchall()
+
+#IMPRIME el resultado de la consulta 02
+print("Usuarios con edad 30:")
+for usuario in usuarios_30:
+    print(usuario)
+
+#Imprime el resultado de la consulta 01 
+print("")
+print("Todos los registros")
 cursor.execute("SELECT * FROM usuarios")
 usuarios = cursor.fetchall()
 
 for usuario in usuarios:
     print(usuario)
 
+cursor.close()
 conn.close()
